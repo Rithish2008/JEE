c36820f5086bcc3e4c2c176e21a0a6dba8a13803
Rithish2008 <166804092+Rithish2008@users.noreply.github.com>
2024-04-16 11:57:01 +0530

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,5 @@
+document.getElementById("open-popup-btn").addEventListener("click",function(){document
+.getElementsByClassName("popup")[0].classList.add("active");});
+
+document.getElementById("dismiss-popup-btn").addEventListener("click",function(){document
+.getElementsByClassName("popup")[0].classList.remove("active");});
